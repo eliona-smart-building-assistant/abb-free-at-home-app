e0a032e840666fb9f1d3667771ecc903468270b2
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-10-11 11:05:04 +0200

    Fix formatting error

--- apiservices/api_version_service.go
+++ apiservices/api_version_service.go
@@ -16,12 +16,12 @@
 package apiservices
 
 import (
+	"abb-free-at-home/apiserver"
 	"context"
 	"encoding/json"
 	"io"
 	"net/http"
 	"os"
-	"abb-free-at-home/apiserver"
 
 	"github.com/eliona-smart-building-assistant/go-utils/common"
 	"github.com/eliona-smart-building-assistant/go-utils/log"
